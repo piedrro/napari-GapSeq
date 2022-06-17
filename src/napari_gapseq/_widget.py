e7474da63d856b2456a373cb47ac12f8169ce80b
turnerp <piers.turner@protonmail.com>
2022-06-17 10:14:10 +0100

    cleaned up code, removed old files

--- src/napari_gapseq/_widget.py
+++ src/napari_gapseq/_widget.py
@@ -71,18 +71,13 @@ def find_contours(img):
 
 
 class GapSeqTabWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+
     def __init__(self, viewer: napari.Viewer):
 
         super().__init__()
 
         from napari_gapseq.gapseq_ui import Ui_TabWidget
-        from napari_gapseq.gapseq_plot_ui import Ui_PlotUI
 
-        application_path = os.path.dirname(sys.executable)
         self.viewer = viewer
         self.setLayout(QVBoxLayout())
 
@@ -122,7 +117,6 @@ class GapSeqTabWidget(QWidget):
         self.gapseq_export_traces = self.findChild(QPushButton, "gapseq_export_traces")
 
         self.graph_container.setLayout(QVBoxLayout())
-        # self.graph_container.setMaximumHeight(500)
         self.graph_container.setMinimumWidth(100)
 
         self.canvas = FigureCanvasQTAgg()
@@ -137,12 +131,6 @@ class GapSeqTabWidget(QWidget):
 
         self.import_localisations = self.findChild(QPushButton, "import_localisations")
         self.import_image = self.findChild(QPushButton, "import_image")
-
-        # self.import_localisations.clicked.connect(self.import_localisation_file)
-        # self.import_image.clicked.connect(self.import_image_file)
-
-        # self.canvas.mpl_connect("button_press_event", self.on_press)
-        # self.viewer.dims.events.current_step.connect(self.update_plot)
 
         self.current_coord = None
 
@@ -1129,83 +1117,6 @@ class GapSeqTabWidget(QWidget):
             self.viewer.dims.current_step = new_dims
 
 
-    # def import_localisation_file(self):
-    #
-    #     # desktop = os.path.expanduser("~/Desktop")
-    #     # path, filter = QFileDialog.getOpenFileName(self, "Open Files", desktop, "Files (*)")
-    #
-    #     path = r"C:/napari-gapseq/src/napari_gapseq/dev/20220527_27thMay2022GAP36A/27thMay2022GAPSeq4onebyonesubstrategAPGS8FRETfoursea50nMconce2_GAPSeqonebyoneGAP36AL532Exp200.tif"
-    #
-    #     image = tifffile.imread(path)
-    #
-    #     image = image[:, :, : image.shape[2] // 2]
-    #
-    #     img = np.max(image, axis=0)
-    #
-    #     img = normalize99(img)
-    #     img = rescale01(img) * 255
-    #     img = img.astype(np.uint8)
-    #
-    #     img = cv.fastNlMeansDenoising(img, h=30, templateWindowSize=5, searchWindowSize=31)
-    #
-    #     _, mask = cv.threshold(img, 150, 255, cv.THRESH_BINARY)
-    #
-    #     contours = find_contours(mask)
-    #
-    #     polygons = []
-    #
-    #     for i in range(len(contours)):
-    #
-    #         try:
-    #
-    #             cnt = contours[i]
-    #
-    #             x, y, w, h = cv.boundingRect(cnt)
-    #
-    #             M = cv.moments(cnt)
-    #             cx = int(M['m10'] / M['m00'])
-    #             cy = int(M['m01'] / M['m00'])
-    #
-    #             polygon = np.array([[cy - 2, cx - 2], [cy + 2, cx + 2]])
-    #
-    #             polygons.append(polygon)
-    #
-    #             # cv.rectangle(img, (cx - 2, cy - 2), (cx + 2, cy + 2), (0, 128, 255), 2)
-    #
-    #         except:
-    #             pass
-    #
-    #
-    #     self.localisation_layer = self.viewer.add_image(image, name="Localisations")
-    #     self.localisation_layer.mouse_drag_callbacks.append(partial(self.select_localisation,mode="click"))
-    #
-    #     self.box_layer = self.viewer.add_shapes(polygons, shape_type='Rectangle', edge_width=1, edge_color='red', face_color=[0,0,0,0], opacity=0.3)
-    #     self.box_layer.mouse_drag_callbacks.append(partial(self.select_localisation,mode="click"))
-    #
-    #     self.viewer.layers.selection.events.changed.connect(partial(self.select_localisation,mode="layer_change"))
-
-    # def import_image_file(self):
-    #
-    #     path = r"C:/napari-gapseq/src/napari_gapseq/dev/20220527_27thMay2022GAP36A/27thMay2022GAPSeq4onebyonesubstrategAPGS8FRETfoursea25nM_GAPSeqonebyoneGS83TL638Exp200.tif"
-    #
-    #     gap_codes = ["A","T","C","G"]
-    #     layer_names = []
-    #     for i in range(len(gap_codes)):
-    #
-    #         index = len(path) - 15
-    #         path = path[:index] + gap_codes[i] + path[index + 1:]
-    #
-    #         image = tifffile.imread(path)
-    #
-    #         image = image[:, :, image.shape[2] // 2 : ]
-    #
-    #         layer_name = f"SEQ-{gap_codes[i]}"
-    #         layer_names.append(layer_name)
-    #
-    #         setattr(self, layer_name, self.viewer.add_image(image, name=layer_name))
-    #         self.viewer.layers[layer_name].mouse_drag_callbacks.append(partial(self.select_localisation, mode="click"))
-
-
     def select_localisation(self, viewer=None, event = None,  mode = "click"):
 
         if mode == "click":
@@ -1246,59 +1157,3 @@ class GapSeqTabWidget(QWidget):
                         localisation_data.append({'layer_name':layer_name, 'data':data, 'spot':spot[current_frame]})
 
                 self.plot(self.canvas, plot_data = localisation_data)
-
-
-    # def plot(self, canvas, plot_data, xlabel = "Frame", ylabel = "Intensity"):
-    #
-    #     canvas.figure.clf()
-    #
-    #     maximum_width = 200*len(plot_data)
-    #
-    #     if maximum_width != 0:
-    #         self.graph_container.setMaximumHeight(maximum_width)
-    #
-    #     for i in range(len(plot_data)):
-    #
-    #         plot_index = int(f"{len(plot_data)}1{i+1}")
-    #         # spot_index = int(f"{len(plot_data)}2{i+2}")
-    #
-    #         axes = canvas.figure.add_subplot(plot_index)
-    #         layer_name = plot_data[i]["layer_name"]
-    #         spot = plot_data[i]["spot"]
-    #
-    #         y = plot_data[i]["data"]
-    #         x = np.arange(len(y))
-    #
-    #         axes.set_facecolor("#262930")
-    #         axes.plot(x, y, label = layer_name)
-    #         axes.legend()
-    #
-    #         ymin, ymax = axes.get_ylim()
-    #         current_frame = int(self.viewer.dims.current_step[0])
-    #
-    #         axes.plot([current_frame,current_frame],[ymin, ymax], color = "red", label = "Frame")
-    #
-    #         # spot_axes = canvas.figure.add_subplot(spot_index)
-    #         # spot_axes.imshow(spot)
-    #
-    #     self.canvas.figure.tight_layout()
-    #     canvas.draw()
-
-    # def update_plot(self):
-    #
-    #     allaxes = self.canvas.figure.get_axes()
-    #
-    #     if len(allaxes) > 0:
-    #
-    #         current_frame = int(self.viewer.dims.current_step[0])
-    #
-    #         for ax in allaxes:
-    #
-    #             # print(ax.get_gridspec())
-    #
-    #             vertical_line = ax.lines[1]
-    #             vertical_line.set_xdata([current_frame, current_frame])
-    #             self.canvas.draw()
-    #             self.canvas.flush_events()
-    #
-    #
